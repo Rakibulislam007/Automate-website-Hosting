93608ebbacab14ce1ff18f421b834679c81cc516
Rakibul Islam <mdrakib079@gmail.com>
2024-01-04 14:24:01 +0000

    Add files via upload

---
+++ Websetup_with_variable.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Variable Declaration
+
+PACKAGE=" wget unzip httpd"
+SVC="httpd"
+URL='https://www.tooplate.com/zip-templates/2137_barista_cafe.zip'
+ART_NAME='2137_barista_cafe'
+TEMPDIR="/tmp/webfiles"
+
+#Installing Dependencies
+echo "#################################"
+echo "installing packeges"
+echo "#################################"
+sudo yum install $PACKAGE -y > /dev/null
+
+#Start AND enable services
+echo "#################################"
+echo "Start & Enable Httpd services"
+echo "#################################"
+
+sudo systemctl start $SVC
+sudo systemctl enable $SVC
+echo
+
+#Creating temp directory
+echo "#################################"
+echo "Starting artifact deployment"
+echo "#################################"
+
+mkdir -p $TEMPDIR
+cd $TEMPDIR
+echo
+
+wget $URL > /dev/null
+
+unzip $ART_NAME.zip > /dev/null
+
+sudo cp -r $ART_NAME/* /var/www/html/
+
+echo "#################################"
+echo "Restarting Httpd services"
+echo "#################################"
+
+
+systemctl restart $SVC
+
+#Cleanup
+echo "#################################"
+echo "Removing temporary file"
+echo "#################################"
+
+rm -rf $TEMPDIR
+
+sudo systemctl status httpd
+ls /var/www/html/
---
+++ websetup.sh
@@ -0,0 +1,50 @@
+# Automate-website-Hosting
+#!/bin/bash
+
+#Installing Dependencies
+echo "#################################"
+echo "installing packeges"
+echo "#################################"
+sudo yum install wget unzip httpd -y > /dev/null
+
+#Start AND enable services
+echo "#################################"
+echo "Start & Enable Httpd services"
+echo "#################################"
+
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo
+
+#Creating temp directory
+echo "#################################"
+echo "Starting artifact deployment"
+echo "#################################"
+
+mkdir -p /tmp/webfiles
+                                                             
+cd /tmp/webfiles
+
+wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /dev/null
+
+unzip 2137_barista_cafe.zip > /dev/null
+
+sudo cp -r 2137_barista_cafe/* /var/www/html/
+
+echo "#################################"
+echo "Restarting Httpd services"
+echo "#################################"
+
+
+systemctl restart httpd
+
+#Cleanup
+echo "#################################"
+echo "Removing temporary file"
+echo "#################################"
+
+rm -rf /tmp/webfiles
+#show status of the service
+sudo systemctl status httpd
+#show content of html
+ls /var/www/html/
